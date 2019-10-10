b4110838c0c0f9339f5ef4c97d33f11c838d433a
Saqib Nazir <saqib_9090@yahoo.com>
2019-10-10 22:03:24 +0200

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 1
+        },
+        {
+          name: 'Dracul',
+          id: 2
+        },
+        {
+          name: 'zombie',
+          id: 3
+        }
+      ]
+    };
+  }
 
+  render() {
+    return (
+      <div>
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
+}
 export default App;
