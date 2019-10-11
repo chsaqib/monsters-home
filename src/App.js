d0ea322f82dfad2470097cef064854235c6b7235
Saqib Nazir <saqib_9090@yahoo.com>
2019-10-11 19:54:17 +0200

    search field

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
   componentDidMount() {
@@ -17,6 +18,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={e => this.setState({ searchField: e.target.value })}
+        ></input>
         <CardList monsters={this.state.monsters}></CardList>
       </div>
     );
