17560d2578c0530c58de45eed4da10bc4f80e07b
Saqib Nazir <saqib_9090@yahoo.com>
2019-10-13 16:03:06 +0200

    Updates

---
+++ precache-manifest.a23583dea96a18e44952c3d8be520a7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f277555fe5e61e65c9732d094b240ef0",
+    "url": "/monsters-home/index.html"
+  },
+  {
+    "revision": "6c29fe515116b63997b3",
+    "url": "/monsters-home/static/css/main.a366f655.chunk.css"
+  },
+  {
+    "revision": "0e24f7de5d98616ce093",
+    "url": "/monsters-home/static/js/2.1622286a.chunk.js"
+  },
+  {
+    "revision": "6c29fe515116b63997b3",
+    "url": "/monsters-home/static/js/main.952e0b85.chunk.js"
+  },
+  {
+    "revision": "61f928c04969c3ac56a9",
+    "url": "/monsters-home/static/js/runtime-main.7f2f6d0e.js"
+  }
+]);
